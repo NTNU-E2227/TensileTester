716080dd1d967240bba36fd83556eb16197c2cf4
vinzenz <vinzenzschoeberle@gmail.com>
2022-04-26 21:43:03 +0200

    Engineering strain

--- service/backend.py
+++ service/backend.py
@@ -5,13 +5,12 @@ import serial.tools.list_ports
 import time
 import json
 import numpy as np
-from decimal import Decimal
+
 class com_obj:
     def __init__(self):
         self.port = None
         self.portList = []
         self.direction = b"l"
-        self.linear = [True,0,0]
         self.running = False
         self.timer_run = False
         self.speed = 0
@@ -98,8 +97,8 @@ class com_obj:
                 yield False
                 continue
             length = self.length_from_raw(data[0])
-            force = self.force_from_raw(data[1])              #engStrain
-            self.latest_data = [self.time(),length,force,self.strain(force,length),self.stress(force)]
+            force = self.force_from_raw(data[1])
+            self.latest_data = [self.time(),length,force,self.strain(length),self.stress(force)]
             if self.datalog:
                 self.datalist[0].append(self.latest_data[0])
                 self.datalist[1].append(self.latest_data[1])
@@ -115,7 +114,6 @@ class com_obj:
 
     def reset_data(self):
         self.datalist = [[],[],[],[],[]]
-        self.linear = [True,0,0]
 
     def set_length_zero(self):
         self.length_zero = self.latest_data[1] + self.length_zero 
@@ -151,80 +149,7 @@ class com_obj:
         stress = force/A0 #in MPa
         return round(stress, 2)
 
-    def strain(self, force, distance):  # Går ut ifra at metallene strekker seg lineært med påført kraft når elastiske.
-        if self.conf["L1"] == 0 or self.conf["H1"] == 0:
-            return (distance / 1000) / self.conf["L0"]
-        elif self.conf["L1"] - self.conf["L0"] > 0 and self.speed != 0 :  # Sjekker om utregninger er nødvendige
-            R0 = (self.conf["H1"] - self.conf["H0"]) / 2
-            R0_L = 0
-            for i in range(89):
-                R0_L += R0 * (math.cos(math.radians(271 + i)) - math.cos(math.radians(270 + i))) * (
-                        self.conf["H0"] / (self.conf["H0"] + 2 * R0 * (1 + math.sin(math.radians(270.5 + i)))))
-            linear_gauge_distance = distance * (self.conf["L0"] - 2 * R0) / ((self.conf["H0"] / self.conf["H1"]) * ( #Lengde i mikrometer
-                    self.conf["L1"] - self.conf["L0"]) + (self.conf["L0"] - 2 * R0) + 2 * R0_L)
-            # distance er strukket lengde dvs. forskjellen på prøvens lengde før og under spenning(ikke elektrisk men fysisk).
-
-            def check_linear(Force_list, Length_list):
-                if len(Force_list)<50:
-                    print("linear1")
-                    return True
-                elif Force_list[-50] < 0:
-                    print("linear1")
-                    return True
-                print("Force:", Force_list[-1], Force_list[-11], Force_list[-21], Force_list[-5], Force_list[-15])
-                print("Length:", Length_list[-1],Length_list[-11],Length_list[-21],Length_list[-5],Length_list[-15])
-                try:
-                    if (Decimal(Force_list[-1] - Force_list[-11]) / Decimal(Length_list[-1] - Length_list[-11])) / (
-                            Decimal(Force_list[-11] - Force_list[-21]) / Decimal(Length_list[-11] - Length_list[-21])) < 0.5:
-                        if (Decimal(Force_list[-5] - Force_list[-15]) / Decimal(Length_list[-5] - Length_list[-15])) /(
-                                Decimal(Force_list[-15] - Force_list[-25]) / Decimal(Length_list[-15] - Length_list[-25])) < 0.5:
-                            return False
-                        else:
-                            return True
-                    else:
-                        return True
-                except:
-                    return True
-
-            if check_linear(self.datalist[2], self.datalist[1]) == False:
-                print("\n NONLINEAR\n")
-                if self.linear[0] == True:
-                    n_max = len(self.datalist[4])-1
-                    n_min = 1
-                    for i in range((len(self.datalist[4]))):
-                        if self.datalist[4][i] > self.datalist[4][n_max]:
-                            n_max = i
-                        if self.datalist[4][i] < self.datalist[4][n_min]:
-                            n_min = i
-                    y_min = self.datalist[4][n_min] + ((self.datalist[4][n_max]-self.datalist[4][n_min])/3)
-                    y_max = self.datalist[4][n_min] + 2 * ((self.datalist[4][n_max]-self.datalist[4][n_min])/3)
-                    y_min_n = 0
-                    y_max_n = 0
-                    for i in range((len(self.datalist[4]))):
-                        if self.datalist[4][i] > y_min:
-                            y_min_n = i
-                    for i in range((len(self.datalist[4]))-y_min_n):
-                        if self.datalist[4][i+y_min_n] > y_max:
-                            y_max_n = i
-                    self.linear[1] = (self.datalist[4][y_max_n]-self.datalist[4][y_min_n])/(self.datalist[3][y_max_n]-self.datalist[3][y_min_n])
-                    self.linear[2] = self.datalist[4][y_max_n]-self.linear[1]*self.datalist[3][y_max_n]
-                self.linear[0] = False
-
-            if self.linear[0]:  # Når Metallet er elastisk
-                print("linear2")
-                return (linear_gauge_distance/1000) / (self.conf["L0"] - 2 * R0)
-            else:  # Når metallet ikke lenger er i elastisk området
-                print("nonlinear2")
-                try:
-                    non_gauge_length = 1 - linear_gauge_distance/distance
-                except ZeroDivisionError: #Hvis distance faktisk er null så er det ikke noe strain
-                    return 0
-                non_gauge_distance = (non_gauge_length/(1-non_gauge_length))*1000*(self.conf["L0"] - 2 * R0)*(((force/(self.conf["E0"]*self.conf["H0"]))-self.linear[2])/self.linear[1])
-                return ((distance-non_gauge_distance)/1000) / (self.conf["L0"] - 2 * R0)
-        else:
-            return (distance/1000) / self.conf["L0"]
-
-    def engStrain(self, dist):
+    def strain(self, dist):
         return round(dist / (self.conf["L0"] * 1e3), 4)
 
     def export(self, loc):
